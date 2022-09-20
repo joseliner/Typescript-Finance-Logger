2bd7c9c2c2e2568fb6c10ba345740fee7275401a
Jos Codes <joselineruiz917@gmail.com>
2022-09-20 09:43:54 -0500

    type aliases

--- sandbox.js
+++
@@ -1,7 +0,0 @@
-var character = 'John';
-console.log(character);
-var inputs = document.querySelectorAll('input');
-console.log(inputs);
-inputs.forEach(function (input) {
-    console.log(input);
-});
