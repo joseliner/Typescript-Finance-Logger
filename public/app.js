bf1fbe17410e7dce8c4b849a96c1ff65b26f8688
Jos Codes <joselineruiz917@gmail.com>
2022-09-21 13:11:42 -0500

    type casting

---
+++ public/app.js
@@ -0,0 +1,11 @@
+"use strict";
+// Type Casting For Inputs
+const form = document.querySelector('.new-item-form');
+const type = document.querySelector('#type');
+const toFrom = document.querySelector('#toFrom');
+const details = document.querySelector('#details');
+const amount = document.querySelector('#amount');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log(type.value, toFrom.value, details.value, amount.valueAsNumber);
+});
