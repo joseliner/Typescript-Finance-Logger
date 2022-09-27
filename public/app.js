4209a48af3280eda77365650fa403642ebca01a4
Jos Codes <joselineruiz917@gmail.com>
2022-09-27 13:35:00 -0500

    added Interfaces

--- public/app.js
+++ public/app.js
@@ -1,6 +1,30 @@
-"use strict";
-// Type Casting For Inputs
+const me = {
+    name: 'Joseline',
+    age: 30,
+    speak(text) {
+        console.log(text);
+    },
+    spend(amount) {
+        console.log('I spent', amount);
+        return amount;
+    }
+};
+const greetPerson = (person) => {
+    console.log('hello ', person.name);
+};
+greetPerson(me);
+console.log(me);
+import { Invoice } from './classes/Invoice.js';
+const invOne = new Invoice('Chris', 'work on the front end of the website', 250);
+const invTwo = new Invoice('Jasmine', 'work on the back end of the website', 350);
+let invoices = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+invoices.forEach(inv => {
+    console.log(inv.client, inv.amount, inv.format());
+});
 const form = document.querySelector('.new-item-form');
+// inputs
 const type = document.querySelector('#type');
 const toFrom = document.querySelector('#toFrom');
 const details = document.querySelector('#details');
--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,49 @@
-// Type Casting For Inputs
+// interfaces
+interface IsPerson {
+    name: string;
+    age: number;
+    speak(a: string): void;
+    spend(a: number): number;
+}
+
+const me: IsPerson = {
+    name: 'Joseline',
+    age: 30,
+    speak(text: string): void {
+        console.log(text);
+    },
+    spend(amount: number): number {
+        console.log('I spent', amount);
+        return amount;
+    }
+};
+
+const greetPerson = (person: IsPerson) => {
+    console.log('hello ', person.name);
+}
+
+greetPerson(me);
+console.log(me);
+
+
+import {Invoice} from './classes/Invoice.js'
+
+const invOne = new Invoice('Chris', 'work on the front end of the website', 250);
+const invTwo = new Invoice('Jasmine', 'work on the back end of the website', 350);
+
+let invoices: Invoice[] = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+
+invoices.forEach(inv => {
+    console.log(inv.client, inv.amount, inv.format());
+});
+
+
+
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
 
+// inputs
 const type  = document.querySelector('#type') as HTMLSelectElement;
 const toFrom  = document.querySelector('#toFrom') as HTMLInputElement;
 const details  = document.querySelector('#details') as HTMLInputElement;
